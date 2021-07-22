b0239c8d06374b450400c2e61664132f9b7f19fa
deathquin <deathquin@gmail.com>
2021-07-22 16:33:15 +0900

    feat: 접근한 사용자의 정보를 가져올 수 있는 정보 추가
    - 접근한 사용자의 정보를 가져올 수 있는 정보 추가
     기존의 GetSystemInfo 함수를 사용
    
    If you use an issue tracker, put references to them at the bottom
    
    Resolves: -
    See also: -

---
+++ encryption/systmem_info.go
@@ -0,0 +1,76 @@
+package helper
+
+import (
+	"fmt"
+	"github.com/mileusna/useragent"
+	"github.com/oschwald/geoip2-golang"
+	"net"
+)
+
+type systemInfo struct {
+	DeviceName      string
+	PlatformName    string
+	PlatformVersion string
+	OS              string
+	OSVersion       string
+	Country         string
+	CountrISOCode   string
+}
+
+// https://www.maxmind.com/en/accounts/542317/geoip/downloads
+func GetSystemInfo(useragent string, clientIP string) systemInfo {
+
+	db, err := geoip2.Open("./encryption/GeoLite2-City.mmdb")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer db.Close()
+
+	ip := net.ParseIP(clientIP)
+	record, err := db.City(ip)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	ua := ua.Parse(useragent)
+
+	var deviceSort string
+
+	if ua.Mobile {
+		deviceSort = "Mobile"
+	}
+	if ua.Tablet {
+		deviceSort = "Tablet"
+	}
+	if ua.Desktop {
+		deviceSort = "Desktop"
+	}
+	if ua.Bot {
+		deviceSort = "Bot"
+	}
+
+	info := systemInfo{
+		DeviceName:      deviceSort,
+		PlatformName:    ua.Name,
+		PlatformVersion: ua.Version,
+		OS:              ua.OS,
+		OSVersion:       ua.OSVersion,
+		Country:         record.Country.Names["en"],
+		CountrISOCode:   record.Country.IsoCode,
+	}
+
+	/*fmt.Println(ua)
+	fmt.Println(ua.Name)
+	fmt.Println(ua.Version)
+	fmt.Println(ua.OS)
+	fmt.Println(ua.OSVersion)
+	fmt.Printf("Russian country name: %v\n", record.Country.Names["en"])
+	fmt.Printf("ISO country code: %v\n", record.Country.IsoCode)
+	fmt.Printf("Time zone: %v\n", record.Location.TimeZone)
+	fmt.Printf("Coordinates: %v, %v\n", record.Location.Latitude, record.Location.Longitude)*/
+
+	return info
+
+}
